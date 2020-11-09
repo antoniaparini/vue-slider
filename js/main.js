4c1e3aabe6290542eb9a3046d26224ca319c7e02
Antonia P <direiellie@gmail.com>
2020-11-09 16:34:56 +0100

    Vue slider complete

---
+++ js/main.js
@@ -0,0 +1,50 @@
+/*console.log('Ready!');<--Debug*/
+
+/**
+ * VUE JS
+ */
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        indexPhoto: 0,
+        photos: [
+            './img/image1.jpg',
+            './img/image2.jpg',
+            './img/image3.jpg',
+            './img/image4.jpg',
+        ],
+        intervalID: 0
+    },
+    created() { 
+        this.startLoop();
+    },
+    methods: {
+        nextPhoto() {
+            this.indexPhoto += 1; //Fattibile anche con this.indexPhoto ++;
+            if (this.indexPhoto > (this.photos.length - 1)) {
+                this.indexPhoto = 0;
+            }
+        },
+
+        prevPhoto() {
+           this.indexPhoto -= 1; //Fattibile anche con this.indexPhoto --;
+           if (this.indexPhoto < 0) {
+               this.indexPhoto = (this.photos.length - 1);
+           }
+       },
+
+       setPhoto(index) {
+           this.indexPhoto = index;
+       },
+
+       startLoop() {
+           this.intervalId = setInterval(()=> {
+               this.nextPhoto();
+           }, 3000);
+       },
+       stopLoop() {
+           clearInterval(this.intervalId);
+       }
+    }
+});
